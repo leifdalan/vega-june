5995347022d121daf259336828a14ad8505e490c
Leif Dalan <leif@tune.com>
2016-07-16 16:04:52 -0700

    Added env definitions.

---
+++ config/custom-environment-variables.js
@@ -0,0 +1,12 @@
+
+
+module.exports = {
+  host: 'HOST',
+  port: 'PORT',
+  apiHost: 'API_HOST',
+  apiPort: 'API_PORT',
+  redisHost: 'REDIS_HOST',
+  redisPort: 'REDIS_PORT',
+  authSecret: 'AUTH_SECRET',
+  redisSecret: 'REDIS_SECRET'
+};
